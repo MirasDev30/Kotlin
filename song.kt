162b9ef259e49c26f4eb60934553cfa750af5da1
MirasDev30 <73632445+MirasDev30@users.noreply.github.com>
2020-10-29 13:02:35 +0600

    Add files via upload

---
+++ song.kt
@@ -0,0 +1,29 @@
+	var author : String = "Marshall Bruce Mathers III"
+        /*
+        * Eminem
+        * Slim Shady
+        * Double M
+        * M&M
+        * */
+        var other_nickname : String = "Eminem"
+        var other_nickname2 : String = "Slim Shady"
+        var other_nickname3 : String = "Double M"
+        var other_nickname4 : String = "M&M"
+        //Date when career started
+        var start_of_career : String = "1988"
+        //Earned wealth
+        var net_worth_in_dollors : Long = 2300000000
+        //Wife and mother of his children
+        var spouse : String = "Kimberly Anne Scott"
+        //He has 3 kids
+        var children : Int  = 3
+
+        println(author)
+        println(other_nickname)
+        println(other_nickname2)
+        println(other_nickname3)
+        println(other_nickname4)
+        println(start_of_career)
+        println(net_worth_in_dollors)
+        println(spouse)
+        println(children)
